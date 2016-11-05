c4476f2e31a8753bdeb887aa1adb2d67df0a7f3e
laatu <laatuji@gmail.com>
2016-11-05 17:44:42 +0530

    Casting to AppImageButton

--- app/src/main/java/com/example/laatuji/a4nov_login/FirstActivity.java
+++ app/src/main/java/com/example/laatuji/a4nov_login/FirstActivity.java
@@ -10,22 +10,28 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.support.v7.widget.AppCompatImageButton;
 
 import com.example.laatuji.a4nov_login.LoginActivity;
 
 public class FirstActivity extends AppCompatActivity {
-
-    Button b1,b2;
+// AppCompactImage
+//   Button b1,b2;
+    AppCompatImageButton b1,b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
+
+        b1=  (AppCompatImageButton) findViewById(R.id.ib1);
+        b2 = (AppCompatImageButton) findViewById(R.id.ib2);
+
 //        Erronious
 
-        b1= (Button) findViewById(R.id.ib1);
-        b2 = (Button) findViewById(R.id.ib2);
+//        b1= (Button) findViewById(R.id.ib1);
+//        b2 = (Button) findViewById(R.id.ib2);
 
     }
     public void adminClicked(View view){

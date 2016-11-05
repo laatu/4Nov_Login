c22d848c6ccc385ab9f391c56890a3ea84e9efe2
laatu <laatuji@gmail.com>
2016-11-05 16:06:47 +0530

    Necessray files are added

---
+++ app/src/main/java/com/example/laatuji/a4nov_login/FirstActivity.java
@@ -0,0 +1,40 @@
+package com.example.laatuji.a4nov_login;
+
+/**
+ * Created by laatuji on 11/4/16.
+ */
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.laatuji.a4nov_login.LoginActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+    Button b1,b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+//        Erronious
+
+        b1= (Button) findViewById(R.id.ib1);
+        b2 = (Button) findViewById(R.id.ib2);
+
+    }
+    public void adminClicked(View view){
+        Intent i =new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(i);
+
+    }
+    public void userClicked(View view){
+        Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
+    }
+}
